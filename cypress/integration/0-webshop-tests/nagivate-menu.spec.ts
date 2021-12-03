33b61396c2e613200663aafb64fd77c706c15d2c
Simon Sandahl <sandahl.simon@hotmail.se>
2021-12-03 09:13:16 +0100

    added some tests

--- cypress/integration/0-webshop-tests/nagivate-menu.spec.ts
+++ cypress/integration/0-webshop-tests/nagivate-menu.spec.ts
@@ -3,9 +3,29 @@ describe("Test if you can navigate to menu through homepage", () => {
         cy.visit("http://localhost:1234");
     });
 
-    it("find menu button and navigate to the product page", () => {
+    it("find menu button and navigate to the productpage", () => {
         cy.get("div#hamburger-btn").click();
         cy.get("ul#dropdown-menu").children().should("have.length", 4);
         cy.get("ul li").contains("Shop").click();
+        cy.url().should("eq", "http://localhost:1234/pages/productpage.html");
+    });
+
+    it("find menu button and navigate to the homepage", () => {
+        cy.get("div#hamburger-btn").click();
+        cy.get("ul#dropdown-menu").children().should("have.length", 4);
+        cy.get("ul li").contains("Home").click();
+        cy.url().should("eq", "http://localhost:1234/index.html");
+    });
+
+    it("find menu button and navigate to the aboutpage", () => {
+        cy.get("div#hamburger-btn").click();
+        cy.get("ul#dropdown-menu").children().should("have.length", 4);
+        cy.get("ul li").contains("About").click();
+    });
+
+    it("find menu button and navigate to the accountpage", () => {
+        cy.get("div#hamburger-btn").click();
+        cy.get("ul#dropdown-menu").children().should("have.length", 4);
+        cy.get("ul li").contains("Account").click();
     });
 });
