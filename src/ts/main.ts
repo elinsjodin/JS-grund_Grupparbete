68ce9f38b4b2e1f52643d9ab20bb530ae739e01f
Simon Sandahl <sandahl.simon@hotmail.se>
2021-12-03 13:41:38 +0100

    fixed cartbutton to navbar + implemented in files

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,21 +1,5 @@
-window.onload = function() {
-    hamburgerMenu();
-}
+import { myNavBar } from "./navbar";
 
-function hamburgerMenu() {
-    const hamburgerBtn: HTMLDivElement = document.getElementById("hamburger-btn") as HTMLDivElement;
-    const dropdownMenu: HTMLUListElement = document.getElementById("dropdown-menu") as HTMLUListElement;
-    let hamburgerBtnOpen: boolean = false;
-    hamburgerBtn.addEventListener("click", () => {
-        if(!hamburgerBtnOpen){
-            hamburgerBtn.classList.add("is-open");
-            dropdownMenu.classList.add("is-down");
-            hamburgerBtnOpen = true;
-        }
-        else{
-            hamburgerBtn.classList.remove("is-open");
-            dropdownMenu.classList.remove("is-down");
-            hamburgerBtnOpen = false;
-        }
-    });
+window.onload = function() {
+    myNavBar();
 }
