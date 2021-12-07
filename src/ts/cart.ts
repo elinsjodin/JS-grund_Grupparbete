c69babccf7f2fa9f0a1754ff644a7ce51633339b
elinsjodin <elinsjodin@outlook.com>
2021-12-07 10:59:30 +0100

    implemented functionality for add item button

--- src/ts/cart.ts
+++ src/ts/cart.ts
@@ -2,6 +2,8 @@ import { myNavBar } from "./navbar";
 
 import { Product } from "./models/products";
 
+import { perfumeimage } from "../components/perfumebottle.jpg";
+
 window.onload = function () {
   loadCart();
   myNavBar();
@@ -9,6 +11,31 @@ window.onload = function () {
 
 let LOCAL_STORAGE_CART_KEY = "product.items";
 let products = JSON.parse(localStorage.getItem(LOCAL_STORAGE_CART_KEY));
+
+let addBtn: HTMLButtonElement = document.createElement("button");
+addBtn.id = "addBtn";
+addBtn.addEventListener("change", () => {
+  incrementCart();
+});
+
+let removeBtn: HTMLButtonElement = document.createElement("button");
+removeBtn.id = "removeBtn";
+removeBtn.addEventListener("change", decrementCart);
+
+let cart = [];
+
+function incrementCart(img: string, name: string, price: string) {
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].name === name) {
+      cart[i].qty += 1;
+      return;
+    }
+  }
+  let item = { img, name, price, qty: 1 };
+  cart.push(item);
+}
+
+function decrementCart() {}
 
 function loadCart() {
   let btnDiv: HTMLDivElement = document.createElement("div");
