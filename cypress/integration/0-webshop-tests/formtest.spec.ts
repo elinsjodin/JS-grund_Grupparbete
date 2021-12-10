0e15900580f4ff460348bb12dac86f6834dacf4b
elinsjodin <elinsjodin@outlook.com>
2021-12-10 11:24:40 +0100

    Implemented accurate test for checkout form

--- cypress/integration/0-webshop-tests/formtest.spec.ts
+++ cypress/integration/0-webshop-tests/formtest.spec.ts
@@ -3,6 +3,15 @@ describe("Test form inputs and validation", () => {
     cy.visit("/pages/checkout.html");
     cy.get("#country-select").select("Sweden");
     cy.get("#first-name").type("Hello, World{enter}");
+    cy.get("#last-name").type("Hello, World{enter}");
+    cy.get("#adress").type("Hello, World{enter}");
+    cy.get("#postal-code").type("Hello, World{enter}");
+    cy.get("#city").type("Hello, World{enter}");
+    cy.get("#email").type("npl@email.com{enter}");
+    cy.get("#phone").type("xxx-xxxxxxx{enter}");
+    cy.get("#discount-code").type("Hello, World{enter}");
+    cy.get("#payment-method-select").select("Card");
+    cy.get(".btn-confirm").click();
   });
   it("should test form submit and validation, should return validation error since no fields are filled in", () => {
     cy.visit("/pages/checkout.html");
