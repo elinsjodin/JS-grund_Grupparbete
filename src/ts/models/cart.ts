9cfa21b0c7f901a13938d870f752ec1079032a08
elinsjodin <elinsjodin@outlook.com>
2021-12-10 15:05:49 +0100

    Implemented changes in cart class

--- src/ts/models/cart.ts
+++ src/ts/models/cart.ts
@@ -3,128 +3,138 @@ import { Item } from "./item";
 import { cartList, localStorageKey } from "../functions/localStorage";
 
 export class Cart {
+  items: Item[];
 
-  constructor(){
-    let cart = cartList || [];
- 
-    function saveInLocalStorage() {
-      localStorage.setItem(localStorageKey, JSON.stringify(Item));
-    }
-    
-    function decrementCart(qtyInput) {
-      let valueCount: number = qtyInput.value;
-    
-      valueCount--;
-    
-      qtyInput.value = valueCount;
-    }
+  constructor() {
+    this.items = cartList || [];
+  }
 
-    function incrementCart(qtyInput) {
-      let valueCount: number = qtyInput.value;
-    
-      valueCount++;
-    
-      qtyInput.value = valueCount;
-    }
+  saveInLocalStorage() {
+    localStorage.setItem(localStorageKey, JSON.stringify(Item));
+  }
 
-    function removeItemFromCart(productToRemove: Product) {
-      for (let i = 0; i < cart.length; i++) {
-        if (cart[i].product.name === productToRemove.name) {
-          cart.splice(i, 1);
-        }
+  decrementCart(qtyInput) {
+    let valueCount: number = qtyInput.value;
+
+    valueCount--;
+
+    qtyInput.value = valueCount;
+  }
+
+  incrementCart(qtyInput) {
+    let valueCount: number = qtyInput.value;
+
+    valueCount++;
+
+    qtyInput.value = valueCount;
+  }
+
+  removeItemFromCart(productToRemove: Product) {
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i].product.name === productToRemove.name) {
+        this.items.splice(i, 1);
       }
     }
+  }
 
-    function quantityChanged(qtyInput) {
-      let input = qtyInput.target;
-      if (isNaN(input.value) || input.value <= 0) {
-        input.value = 1;
-      }
+  quantityChanged(qtyInput) {
+    let input = qtyInput.target;
+    if (isNaN(input.value) || input.value <= 0) {
+      input.value = 1;
     }
-    
-    function updateCartTotal() {
-      let total: number = 0;
-      for (let i = 0; i < cart.length; i++) {
-        total = total + cart[i].product.price * cart[i].qty;
-      }
-      total = Math.round(total * 100) / 100;
-      let totalCartPrice: HTMLSpanElement = document.getElementById(
-        "total-cart-price"
-      ) as HTMLDivElement;
-      totalCartPrice.innerText = total + ":-";
+  }
+
+  updateCartTotal() {
+    let total: number = 0;
+    for (let i = 0; i < this.items.length; i++) {
+      total = total + this.items[i].product.price * this.items[i].qty;
     }
-    };
+    total = Math.round(total * 100) / 100;
+    let totalCartPrice: HTMLSpanElement = document.getElementById(
+      "total-cart-price"
+    ) as HTMLDivElement;
+    totalCartPrice.innerText = total + ":-";
+  }
+  addToCart(productToAdd: Product) {
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i].product.name === productToAdd.name) {
+        this.items[i].qty += 1;
+        return;
+      }
+      let item: Item = {
+        product: productToAdd,
+        qty: 1,
+      };
+      this.items.push(item);
+    }
+    this.saveInLocalStorage();
+  }
 
-//export class Cart{
-//     localStorage: Function;
-//     decrementCart: Function;
-//     incrementCart: Function;
-//     removeItem: Function;
-//     quantity: Function;
-//     updateCart: Function;
+  //export class Cart{
+  //     localStorage: Function;
+  //     decrementCart: Function;
+  //     incrementCart: Function;
+  //     removeItem: Function;
+  //     quantity: Function;
+  //     updateCart: Function;
 
-//     constructor(localStorage, decrementCart, incrementCart, removeItemFromCart, quantityChanged, updateCartTotal){
-//     this.localStorage = localStorage;
-//     this.decrementCart = decrementCart;
-//     this.incrementCart = incrementCart;
-//     this.removeItem = removeItemFromCart;
-//     this.quantity = quantityChanged;
-//     this.updateCart = updateCartTotal;
-//     }
-//     function decrementCart(qtyInput) {
-//   let valueCount: number = qtyInput.value;
+  //     constructor(localStorage, decrementCart, incrementCart, removeItemFromCart, quantityChanged, updateCartTotal){
+  //     this.localStorage = localStorage;
+  //     this.decrementCart = decrementCart;
+  //     this.incrementCart = incrementCart;
+  //     this.removeItem = removeItemFromCart;
+  //     this.quantity = quantityChanged;
+  //     this.updateCart = updateCartTotal;
+  //     }
+  //     function decrementCart(qtyInput) {
+  //   let valueCount: number = qtyInput.value;
 
-//   valueCount--;
+  //   valueCount--;
 
-//   qtyInput.value = valueCount;
+  //   qtyInput.value = valueCount;
 
-//   saveInLocalStorage();
-// }
+  //   saveInLocalStorage();
+  // }
 
-// function incrementCart(qtyInput) {
-//   let valueCount: number = qtyInput.value;
+  // function incrementCart(qtyInput) {
+  //   let valueCount: number = qtyInput.value;
 
-//   valueCount++;
+  //   valueCount++;
 
-//   qtyInput.value = valueCount;
+  //   qtyInput.value = valueCount;
 
-//   saveInLocalStorage();
-// }
+  //   saveInLocalStorage();
+  // }
 
-// function removeItemFromCart(productToRemove: Product) {
-//   for (let i = 0; i < cart.length; i++) {
-//     if (cart[i].product.name === productToRemove.name) {
-//       cart.splice(i, 1);
-//     }
-//   }
-//   updateCartTotal();
-//   saveInLocalStorage();
-// }
+  // function removeItemFromCart(productToRemove: Product) {
+  //   for (let i = 0; i < cart.length; i++) {
+  //     if (cart[i].product.name === productToRemove.name) {
+  //       cart.splice(i, 1);
+  //     }
+  //   }
+  //   updateCartTotal();
+  //   saveInLocalStorage();
+  // }
 
-// function quantityChanged(qtyInput) {
-//   let input = qtyInput.target;
-//   if (isNaN(input.value) || input.value <= 0) {
-//     input.value = 1;
-//   }
-// }
+  // function quantityChanged(qtyInput) {
+  //   let input = qtyInput.target;
+  //   if (isNaN(input.value) || input.value <= 0) {
+  //     input.value = 1;
+  //   }
+  // }
 
-// function updateCartTotal() {
-//   let total: number = 0;
-//   for (let i = 0; i < cart.length; i++) {
-//     total = total + cart[i].product.price * cart[i].qty;
-//   }
+  // function updateCartTotal() {
+  //   let total: number = 0;
+  //   for (let i = 0; i < cart.length; i++) {
+  //     total = total + cart[i].product.price * cart[i].qty;
+  //   }
 
-//   total = Math.round(total * 100) / 100;
-//   let totalCartPrice: HTMLSpanElement = document.getElementById(
-//     "total-cart-price"
-//   ) as HTMLDivElement;
-//   totalCartPrice.innerText = total + ":-";
-// }
-//   }
-//}
-
-
-
-
-
-
+  //   total = Math.round(total * 100) / 100;
+  //   let totalCartPrice: HTMLSpanElement = document.getElementById(
+  //     "total-cart-price"
+  //   ) as HTMLDivElement;
+  //   totalCartPrice.innerText = total + ":-";
+  // }
+  //   }
+  //}
+}
