af42085e8aaabea0fcfeadbb9dbcc3ecc1a04f74
Simon Sandahl <sandahl.simon@hotmail.se>
2021-12-15 19:05:21 +0100

    fixed productpage styling

--- src/ts/productpage.ts
+++ src/ts/productpage.ts
@@ -4,7 +4,7 @@ import { footer } from "./functions/footer";
 import { products } from "./functions/productList";
 import { productList } from "./functions/localStorage";
 import { goToDetailsPage } from "./details";
-import { Cart } from "./models/cart";
+
 
 window.onload = function () {
   myNavBar();
@@ -16,37 +16,44 @@ window.onload = function () {
 
 function start() {
   let productDiv: HTMLDivElement = document.getElementById(
-    "product-div"
+    "hero-div"
   ) as HTMLDivElement;
 
   for (let i = 0; i < productList.length; i++) {
+    let productContainer: HTMLDivElement = document.createElement("div");
+    productContainer.className = "product-div";
+
     let productImg: HTMLImageElement = document.createElement("img");
-    productImg.id = "productImg";
+    productImg.className = "product-img";
     productImg.alt = "Perfumebottle";
     productImg.src = productList[i].img;
-
-    let price: HTMLParagraphElement = document.createElement("p");
-    price.id = "price";
-    price.innerHTML = productList[i].price + ":-".toString();
-
-    let detailsLink: HTMLParagraphElement = document.createElement("p");
-    detailsLink.id = "detailsLink";
-    detailsLink.innerHTML = "View product";
-    detailsLink.addEventListener("click", () => {
+    productImg.addEventListener("click", () => {
       redirect(productList[i].id);
     });
 
+    let price: HTMLParagraphElement = document.createElement("p");
+    price.className = "product-price";
+    price.innerHTML = productList[i].price + "kr".toString();
+
+    // let detailsLink: HTMLParagraphElement = document.createElement("p");
+    // detailsLink.id = "detailsLink";
+    // detailsLink.innerHTML = "View product";
+    // detailsLink.addEventListener("click", () => {
+    //   redirect(productList[i].id);
+    // });
+
     let productName: HTMLHeadElement = document.createElement("h2");
-    productName.id = "productName";
+    productName.className = "product-name";
     productName.innerHTML = productList[i].name;
     productName.addEventListener("click", () => {
       redirect(productList[i].id);
     });
 
-    productDiv.appendChild(productImg);
-    productDiv.appendChild(price);
-    productDiv.appendChild(detailsLink);
-    productDiv.appendChild(productName);
+    productContainer.appendChild(productImg);
+    productContainer.appendChild(productName);
+    productContainer.appendChild(price);
+    productDiv.appendChild(productContainer);
+    // productDiv.appendChild(detailsLink);
   }
 
   localStorage.setItem("Productlist", JSON.stringify(productList));
