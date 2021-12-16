cce079c6ada9224ce8e10169ba7cb9f4c3a8f31b
elinsjodin <elinsjodin@outlook.com>
2021-12-16 12:46:47 +0100

    Implemented styling: mobile & desktop for thankyou

--- src/ts/thankyou.ts
+++ src/ts/thankyou.ts
@@ -10,6 +10,10 @@ window.onload = function () {
     "order-summary"
   ) as HTMLDivElement;
 
+  let hr1: HTMLHRElement = document.createElement("hr");
+  hr1.className = "solid";
+  hr1.id = "hr-1";
+
   let orderQty: HTMLParagraphElement = document.createElement("p");
   orderQty.id = "cart-count";
   orderQty.innerHTML = cart.displayCartQty() + " items";
@@ -18,7 +22,13 @@ window.onload = function () {
   orderTotal.id = "order-total";
   orderTotal.innerHTML = "Total: " + cart.updateCartTotal() + ":-";
 
+  let hr2: HTMLHRElement = document.createElement("hr");
+  hr2.className = "solid";
+  hr2.id = "hr-2";
+
+  orderSum.appendChild(hr1);
   orderSum.appendChild(orderQty);
   orderSum.appendChild(orderTotal);
+  orderSum.appendChild(hr2);
   cart.displayCartQty();
 };
