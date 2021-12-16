6fc29120f4fe51d470633cd519d542e42fec8fcf
elinsjodin <elinsjodin@outlook.com>
2021-12-16 15:19:38 +0100

    Implemented functionality: price total on checkout

--- src/ts/checkout.ts
+++ src/ts/checkout.ts
@@ -1,7 +1,16 @@
 import { myNavBar } from "./functions/navbar";
 import { footer } from "./functions/footer";
+import { Cart } from "./models/cart";
+
+let cart = new Cart();
 
 window.onload = function () {
   myNavBar();
   footer();
+  let totalCheckout = cart.updateCartTotal();
+  let checkoutTotal: HTMLParagraphElement = document.getElementById(
+    "cost-total"
+  ) as HTMLParagraphElement;
+  checkoutTotal.innerHTML = "Total: " + totalCheckout.toString() + ":-";
+  cart.displayCartQty();
 };
